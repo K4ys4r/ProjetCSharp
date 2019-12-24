267145d3499f74bf950a8a2ca53b8b8319a04606
aurorele13 <aurorele13@gmail.com>
2019-12-24 11:45:12 +0100

    Application v1.0

--- ProjetCSharp/QuestionQCM.cs
+++ ProjetCSharp/QuestionQCM.cs
@@ -4,9 +4,9 @@ namespace ProjetCSharp
     public class QuestionQCM
     {
         public int Numéro { get; private set; } // Propriété: Numéro de la question 
-        public string TextQuestion { get; private set; } // Propriété: le texte de la question 
-        public string PossibilitésRéponse { get; private set; } // Propriété : Possibilité de Réponse ,A-B-C-D-AB-AC-AD-BA-BC-BD-CA-CB-CD-DA-DB-DC-ABC-DCD
-        public string RéponseQuestion { get; private set; }
+        public string TextQuestion { get; set; } // Propriété: le texte de la question 
+        public string PossibilitésRéponse { get;  set; } // Propriété : Possibilité de Réponse ,A-B-C-D-AB-AC-AD-BA-BC-BD-CA-CB-CD-DA-DB-DC-ABC-DCD
+        public string RéponseQuestion { get; set; }
         /// <summary>
         /// Constructeur QCM
         /// </summary>
@@ -14,7 +14,7 @@ namespace ProjetCSharp
         /// <param les reponses possibles="réponse"></param>
         /// <param possibilité de réponse="option"></param>
         /// <param Numéro de la question="n"></param>
-        public QuestionQCM(string question,string réponse,string option,int n)
+        public QuestionQCM(int n)
         {
             Numéro = n;
             TextQuestion = string.Empty;
